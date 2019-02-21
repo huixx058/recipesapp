311001c491a9cb4c5148d705e2f8fbb0d85428d7
Justin Hui <huixx058@umn.edu>
2019-02-21 00:12:48 -0600

    change home page

--- src/main/webapp/app/app-routing.module.ts
+++ src/main/webapp/app/app-routing.module.ts
@@ -1,23 +1,18 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { errorRoute, navbarRoute } from './layouts';
 import { DEBUG_INFO_ENABLED } from 'app/app.constants';
 
 const LAYOUT_ROUTES = [navbarRoute, ...errorRoute];
-
+const routes: Routes = [
+    {
+        path: 'admin',
+        loadChildren: './admin/admin.module#RecipesappAdminModule'
+    },
+    ...LAYOUT_ROUTES
+];
 @NgModule({
-    imports: [
-        RouterModule.forRoot(
-            [
-                {
-                    path: 'admin',
-                    loadChildren: './admin/admin.module#RecipesappAdminModule'
-                },
-                ...LAYOUT_ROUTES
-            ],
-            { useHash: true, enableTracing: DEBUG_INFO_ENABLED }
-        )
-    ],
+    imports: [RouterModule.forRoot(routes, { useHash: true, enableTracing: DEBUG_INFO_ENABLED })],
     exports: [RouterModule]
 })
 export class RecipesappAppRoutingModule {}
